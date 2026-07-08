d94ea0171140fdf4cf5381ba96edbcc3a9d0c147
tenere <xeno.02.freecell@gmail.com>
2026-07-08 22:17:35 +0900

    initial commit

---
+++ WinFormsMultiLangSample/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsMultiLangSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LanguageButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Language = "en";
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(
+                "Language changes will take effect after restarting the application.",
+                "Language Setting",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ WinFormsMultiLangSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsMultiLangSample
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // 保存済みの言語設定を読み込む
+            string lang = Properties.Settings.Default.Language;
+            if (string.IsNullOrEmpty(lang))
+            {
+                lang = "en"; // 初回起動時の既定値
+            }
+
+            var culture = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentUICulture = culture;
+            Thread.CurrentThread.CurrentCulture = culture; // 日付や数値の書式も合わせる場合
+
+            Application.Run(new Form1());
+        }
+    }
+}
